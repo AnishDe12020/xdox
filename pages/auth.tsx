0a9d5f48d512d81946af1932b13975e7e7f07231
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-07 15:48:19 +0530

    feat(auth-email): added custom schema validation messages and showed errors

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -7,20 +7,22 @@ import Button from "../components/Button";
 import Divider from "../components/Divider";
 
 const EmailSchema = yup.object().shape({
-  email: yup.string().email().required(),
+  email: yup
+    .string()
+    .email("Input must be a valid e-mail")
+    .required("E-mail is required"),
 });
 
 const AuthPage: NextPage = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm({ resolver: yupResolver(EmailSchema) });
 
-  const onEmailSubmit = handleSubmit((data) => console.log(data));
-  console.log(watch("email")); // watch input value by passing the name of it
   console.log(errors);
+
+  const onEmailSubmit = handleSubmit((data) => console.log(data));
 
   return (
     <div className="flex justify-center items-center mt-64 flex-col">
@@ -34,10 +36,19 @@ const AuthPage: NextPage = () => {
       <Divider>or continue with</Divider>
       <form
         onSubmit={onEmailSubmit}
-        className="flex flex-col space-y-8 justify-center items-center"
+        className="flex flex-col space-y-4 justify-center items-center"
       >
-        <input {...register("email")} placeholder="john.doe@example.com" />
-        <Button type="submit" className="bg-secondary text-white8">
+        <input
+          {...register("email")}
+          placeholder="john.doe@example.com"
+          className="px-4 py-2 rounded-lg bg-secondary focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-60 border-2 border-gray-700 hover:border-opacity-60 transition duration-200"
+        />
+        {errors.email && (
+          <p className="rounded-lg bg-red-500 px-3 py-2 text-xs italic">
+            {errors.email.message}
+          </p>
+        )}
+        <Button type="submit" className="bg-secondary text-white">
           Login with Email
         </Button>
       </form>
