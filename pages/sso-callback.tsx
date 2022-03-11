3e47225006c62512bb5de908a30ef8bb510919ab
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-11 14:43:16 +0530

    chore: remove unused page

--- pages/sso-callback.tsx
+++
@@ -1,8 +0,0 @@
-import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
-import { NextPage } from "next";
-
-const SSOCallbackPage: NextPage = () => {
-  return <AuthenticateWithRedirectCallback />;
-};
-
-export default SSOCallbackPage;
