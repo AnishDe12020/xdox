062934f9575bdde50129f5706e6d29f183b61a75
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-14 15:25:49 +0530

    style(form-group): change padding for switch

--- components/FormGroup.tsx
+++ components/FormGroup.tsx
@@ -45,7 +45,7 @@ const FormGroup = ({
         <SwitchPrimitive.Root
           className={classNames(
             "group radix-state-checked:bg-blue-500 radix-state-unchecked:bg-secondary",
-            "border-2 border-primary px-1",
+            "border-2 border-primary px-[0.25rem]",
             "relative inline-flex h-[30px] w-[48px] flex-shrink-0 cursor-pointer items-center rounded-full",
             "transition duration-200 ease-in-out",
             "focus:outline-none focus-visible:ring-4 focus-visible:ring-blue-500 focus-visible:ring-opacity-60"
@@ -56,7 +56,7 @@ const FormGroup = ({
         >
           <SwitchPrimitive.Thumb
             className={classNames(
-              "group-radix-state-checked:translate-x-4 group-radix-state-unchecked:translate-x-0",
+              "group-radix-state-checked:translate-x-[1.075rem] group-radix-state-unchecked:translate-x-0",
               "pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-accent shadow-lg ring-0 transition duration-200 ease-in-out"
             )}
           />
