ad7764d87acfeb85a2aa7422d20d7c6d386c80c7
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-14 15:17:39 +0530

    feat(form-group): added support for a switch component

--- components/FormGroup.tsx
+++ components/FormGroup.tsx
@@ -1,4 +1,6 @@
 import { UseFormRegister } from "react-hook-form";
+import * as SwitchPrimitive from "@radix-ui/react-switch";
+import classNames from "classnames";
 
 interface IFormGroupProps {
   register: UseFormRegister<any>;
@@ -10,6 +12,8 @@ interface IFormGroupProps {
   label?: string;
   id?: string;
   textarea?: boolean;
+  isSwitch?: boolean;
+  checked?: boolean;
 }
 
 const FormGroup = ({
@@ -20,6 +24,8 @@ const FormGroup = ({
   label,
   id,
   textarea,
+  isSwitch,
+  checked,
 }: IFormGroupProps): JSX.Element => {
   return (
     <div className="flex flex-col justify-center space-y-2">
@@ -35,6 +41,26 @@ const FormGroup = ({
           placeholder={placeholder}
           className="h-24 resize-y rounded-lg border-4 border-gray-900 bg-secondary px-4 py-2 transition duration-200 hover:border-opacity-60 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-60"
         />
+      ) : isSwitch ? (
+        <SwitchPrimitive.Root
+          className={classNames(
+            "group radix-state-checked:bg-blue-500 radix-state-unchecked:bg-secondary",
+            "border-2 border-primary px-1",
+            "relative inline-flex h-[30px] w-[48px] flex-shrink-0 cursor-pointer items-center rounded-full",
+            "transition duration-200 ease-in-out",
+            "focus:outline-none focus-visible:ring-4 focus-visible:ring-blue-500 focus-visible:ring-opacity-60"
+          )}
+          {...register(name)}
+          id={id || name}
+          defaultChecked={checked}
+        >
+          <SwitchPrimitive.Thumb
+            className={classNames(
+              "group-radix-state-checked:translate-x-4 group-radix-state-unchecked:translate-x-0",
+              "pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-accent shadow-lg ring-0 transition duration-200 ease-in-out"
+            )}
+          />
+        </SwitchPrimitive.Root>
       ) : (
         <input
           {...register(name)}
