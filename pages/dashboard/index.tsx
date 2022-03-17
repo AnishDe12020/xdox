e505ddb0eddbad67a5da6abb8f6a8765f9596e39
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-17 15:01:11 +0530

    feat(dashboard): list out challenges in a simple view

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -4,12 +4,22 @@ import { GET_CHALLENGES } from "../../graphql/queries";
 import CreateChallenge from "../../components/CreateChallenge";
 
 const DashboardPage: NextPage = () => {
-  const { data } = useQuery(GET_CHALLENGES);
+  const { data, error } = useQuery(GET_CHALLENGES);
+
+  console.log(data);
 
   return (
     <div className="flex flex-col">
-      <div className="flex w-1/4 flex-col items-end justify-end bg-secondary">
+      <div className="flex w-1/4 flex-col items-start justify-end space-y-8">
         <CreateChallenge />
+        {data?.challenges?.map(challenge => (
+          <div
+            key={challenge.id}
+            className="flex flex-col rounded-lg bg-secondary p-4"
+          >
+            {challenge.days}DaysOf{challenge.topic}
+          </div>
+        ))}
       </div>
     </div>
   );
