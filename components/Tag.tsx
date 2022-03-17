2bccecabba463e7ba719ce97defd0062e02739b4
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-17 15:19:47 +0530

    feat: made a tag component

---
+++ components/Tag.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+interface ITagProps {
+  icon?: ReactNode;
+  children: ReactNode;
+}
+
+const Tag = ({ icon, children }: ITagProps): JSX.Element => {
+  return (
+    <div className="inline-flex w-fit items-center rounded-md bg-accent bg-opacity-20 px-3 py-1 text-xs font-bold uppercase text-accent">
+      {icon}
+      {children}
+    </div>
+  );
+};
+
+export default Tag;
