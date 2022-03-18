106d0e47fbd8379dcf553cbbc6e0cd55f3373640
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-18 14:52:23 +0530

    feat(dashboard): add loading skeleton for when loaidng challenges data

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -6,7 +6,7 @@ import DashboardLayout from "../../layouts/DashboardLayout";
 import Tag from "../../components/Tag";
 
 const DashboardPage: NextPage = () => {
-  const { data, error } = useQuery(GET_CHALLENGES);
+  const { data, error, loading } = useQuery(GET_CHALLENGES);
 
   console.log(data);
 
@@ -14,20 +14,45 @@ const DashboardPage: NextPage = () => {
     <DashboardLayout>
       <div className="flex w-1/4 flex-col items-start justify-end space-y-8">
         <CreateChallenge />
-        {data?.challenges?.map(challenge => (
-          <div
-            key={challenge.id}
-            className="flex h-32 w-48 flex-col space-y-2 rounded-lg bg-secondary p-4"
-          >
-            <p className="text-lg font-semibold">
-              <span className="mr-1 bg-gradient-to-br from-pink-400 from-blue-500 to-blue-400 to-green-500 bg-clip-text text-transparent">
-                {challenge.days}
-              </span>
-              DaysOf<span>{challenge.topic}</span>
-            </p>
-            {challenge.isPublic ? <Tag>Public</Tag> : <Tag>Private</Tag>}
-          </div>
-        ))}
+        {loading ? (
+          <>
+            <div className="flex h-32 w-48 flex-col space-y-2 rounded-lg bg-secondary p-4">
+              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
+              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
+            </div>
+            <div className="flex h-32 w-48 flex-col space-y-2 rounded-lg bg-secondary p-4">
+              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
+              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
+            </div>
+            <div className="flex h-32 w-48 flex-col space-y-2 rounded-lg bg-secondary p-4">
+              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
+              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
+            </div>
+            <div className="flex h-32 w-48 flex-col space-y-2 rounded-lg bg-secondary p-4">
+              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
+              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
+            </div>
+            <div className="flex h-32 w-48 flex-col space-y-2 rounded-lg bg-secondary p-4">
+              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
+              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
+            </div>
+          </>
+        ) : (
+          data?.challenges?.map(challenge => (
+            <div
+              key={challenge.id}
+              className="flex h-32 w-48 flex-col space-y-2 rounded-lg bg-secondary p-4"
+            >
+              <p className="text-lg font-semibold">
+                <span className="mr-1 bg-gradient-to-br from-pink-400 from-blue-500 to-blue-400 to-green-500 bg-clip-text text-transparent">
+                  {challenge.days}
+                </span>
+                DaysOf<span>{challenge.topic}</span>
+              </p>
+              {challenge.isPublic ? <Tag>Public</Tag> : <Tag>Private</Tag>}
+            </div>
+          ))
+        )}
       </div>
     </DashboardLayout>
   );
