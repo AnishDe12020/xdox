c6192e386878a467ff6b5c601cb097b8d256aade
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-19 23:21:05 +0530

    refactor(week-bar): move the week bar to its own component

---
+++ components/WeekBar.tsx
@@ -0,0 +1,37 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
+import useDate from "../hooks/useDate";
+import Button from "./Button";
+import DayButton from "./DayButton";
+
+const WeekBar = (): JSX.Element => {
+  const { getCurrentWeek, date, setDate, formatDate, setDayTo } = useDate();
+
+  return (
+    <>
+      <div className="ml-2 flex items-center space-x-2">
+        <Button
+          className="bg-transparent px-1 py-1 text-accent"
+          onClick={() => setDayTo(-7)}
+        >
+          <ChevronLeftIcon className="h-5 w-5" />
+        </Button>
+        <span>Week {formatDate(date, "WW")}</span>
+        <Button className="bg-transparent px-1 py-1 text-accent">
+          <ChevronRightIcon className="h-5 w-5" onClick={() => setDayTo(7)} />
+        </Button>
+      </div>
+      <div className="flex space-x-4">
+        {getCurrentWeek().map(dateOfWeek => (
+          <DayButton
+            date={dateOfWeek}
+            active={date === dateOfWeek}
+            key={dateOfWeek}
+            onClick={setDate}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default WeekBar;
