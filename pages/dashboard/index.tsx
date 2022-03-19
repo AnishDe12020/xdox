a5f1d3651d842f1bd1d1a1e90af757634a2b2e36
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-19 15:57:56 +0530

    feat(dashboard): made the week switcher fully working by making the chevrons buttons

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,12 +1,13 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 import { NextPage } from "next";
+import Button from "../../components/Button";
 import Challenges from "../../components/Challenges";
 import DayButton from "../../components/DayButton";
 import useDate from "../../hooks/useDate";
 import DashboardLayout from "../../layouts/DashboardLayout";
 
 const DashboardPage: NextPage = () => {
-  const { getCurrentWeek, date, setDate, formatDate } = useDate();
+  const { getCurrentWeek, date, setDate, formatDate, setDayTo } = useDate();
 
   console.log(getCurrentWeek());
 
@@ -15,9 +16,16 @@ const DashboardPage: NextPage = () => {
       <Challenges />
       <div className="flex w-full flex-col">
         <div className="ml-2 flex items-center space-x-2">
-          <ChevronLeftIcon className="h-5 w-5" />
+          <Button
+            className="bg-transparent px-1 py-1 text-accent"
+            onClick={() => setDayTo(-7)}
+          >
+            <ChevronLeftIcon className="h-5 w-5" />
+          </Button>
           <span>Week {formatDate(date, "WW")}</span>
-          <ChevronRightIcon className="h-5 w-5" />
+          <Button className="bg-transparent px-1 py-1 text-accent">
+            <ChevronRightIcon className="h-5 w-5" onClick={() => setDayTo(7)} />
+          </Button>
         </div>
         <div className="flex space-x-4">
           {getCurrentWeek().map(dateOfWeek => (
