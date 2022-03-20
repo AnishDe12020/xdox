c5f122bd532b6e184c1f6292b8ba0c23ff06ef95
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-20 16:41:40 +0530

    feat: fixed editor onChange function

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -22,25 +22,10 @@ const Editor = ({ content, onChange }: IEditorProps) => {
           "prose prose-sm sm:prose lg:prose-lg px-4 py-2 focus:outline-none text-accent prose-default bg-secondary rounded-lg m-6",
       },
     },
-    content: `
-        <p>
-          That’s a boring paragraph followed by a fenced code block:
-        </p>
-        <pre><code class="language-javascript">for (var i=1; i <= 20; i++)
-{
-  if (i % 15 == 0)
-    console.log("FizzBuzz");
-  else if (i % 3 == 0)
-    console.log("Fizz");
-  else if (i % 5 == 0)
-    console.log("Buzz");
-  else
-    console.log(i);
-}</code></pre>
-        <p>
-          Press Command/Ctrl + Enter to leave the fenced code block and continue typing in boring paragraphs.
-        </p>
-      `,
+    content: content,
+    onUpdate: ({ editor }) => {
+      onChange(editor.getJSON());
+    },
   });
 
   console.log(content);
