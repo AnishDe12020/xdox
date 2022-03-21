874800bb06ab48af6f69e3467895b22fe5b0a33e
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-21 15:20:42 +0530

    style(menu-bar): styled it to match overall looks of the app

--- components/Editor/MenuBar.tsx
+++ components/Editor/MenuBar.tsx
@@ -1,5 +1,7 @@
 import { FontBoldIcon, FontItalicIcon } from "@radix-ui/react-icons";
 import type { Editor } from "@tiptap/react";
+import classNames from "classnames";
+import { cloneElement } from "react";
 import Button from "../Button";
 
 interface IMenuBarProps {
@@ -12,28 +14,36 @@ const MenuBar = ({ editor }: IMenuBarProps): JSX.Element => {
       icon: <FontBoldIcon />,
       title: "Bold",
       action: () => editor.chain().focus().toggleBold().run(),
-      isActive: () => editor.isActive("bold"),
+      isActive: editor.isActive("bold"),
     },
     {
       icon: <FontItalicIcon />,
       title: "Italic",
       action: () => editor.chain().focus().toggleItalic().run(),
-      isActive: () => editor.isActive("italic"),
+      isActive: editor.isActive("italic"),
     },
     { type: "separator" },
   ];
   return (
-    <div>
-      <p>hi</p>
-      {items.map((item, index) => 
-        item.type === "separator" ? (
+    <div className="mx-4 flex space-x-2 rounded-lg bg-secondary px-4 py-2">
+      {items.map(({ icon, title, action, isActive, type }, index) =>
+        type === "separator" ? (
           <div key={index} className="separator" />
         ) : (
-          <Button key={index} onClick={item.action}>
-            {item.icon}
+          <Button
+            key={index}
+            onClick={action}
+            className={classNames(
+              "transition-100 rounded-md bg-secondary px-2 py-2 text-accent hover:bg-primary hover:opacity-100",
+              isActive && "bg-primary"
+            )}
+            aria-label={title}
+            title={title}
+          >
+            {icon && cloneElement(icon, { className: "h-4 w-4" })}
           </Button>
         )
-     )}
+      )}
     </div>
   );
 };
