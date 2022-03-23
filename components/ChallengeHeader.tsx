2017ca848963dc23404dc523234338ed6d0ac87b
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-23 12:34:03 +0530

    refactor: move the challenges page header to its own component

---
+++ components/ChallengeHeader.tsx
@@ -0,0 +1,50 @@
+import { useQuery } from "@apollo/client";
+import toast from "react-hot-toast";
+import { GET_CHALLENGE } from "../graphql/queries";
+import { ChallengeData } from "../types/Challenges";
+import Tag from "./Tag";
+
+interface IChallengeHeaderProps {
+id: string;
+}
+
+const ChallengeHeader = ({id}: IChallengeHeaderProps): JSX.Element => {
+
+let { data, loading, error, previousData } = useQuery<ChallengeData>(
+    GET_CHALLENGE,
+    { variables: { id: id } }
+  );
+
+  if (loading) {
+    data = previousData;
+  }
+
+  if (error) {
+    console.error(error);
+    toast.error("Something went wrong!");
+  }
+
+
+return !data ? (
+          <div className="mx-4 mb-16 flex space-x-2 items-center justify-center">
+            <div className="h-8 w-64 animate-pulse rounded-lg bg-gray-500" />
+            <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
+          </div>
+        ) : (
+          <div className="mx-4 mb-16 flex items-center justify-center space-x-2">
+            <p className="items-center text-xl font-semibold md:text-2xl lg:text-3xl">
+              <span className="mr-1 bg-gradient-to-br from-pink-400 to-blue-400 bg-clip-text text-transparent">
+                {data.challenges_by_pk.days}
+              </span>
+              DaysOf<span>{data.challenges_by_pk.topic}</span>
+            </p>
+            {data.challenges_by_pk.isPublic ? (
+              <Tag>Public</Tag>
+            ) : (
+              <Tag>Private</Tag>
+            )}
+          </div>
+        )
+}
+
+export default ChallengeHeader
--- pages/dashboard/[id].tsx
+++ pages/dashboard/[id].tsx
@@ -1,64 +1,22 @@
-import { useQuery } from "@apollo/client";
 import { Content } from "@tiptap/react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import toast from "react-hot-toast";
+import ChallengeHeader from "../../components/ChallengeHeader";
 import Challenges from "../../components/Challenges";
 import Editor from "../../components/Editor";
-import Tag from "../../components/Tag";
 import WeekBar from "../../components/WeekBar";
-import { GET_CHALLENGE } from "../../graphql/queries";
 import DashboardLayout from "../../layouts/DashboardLayout";
-import { ChallengeData } from "../../types/Challenges";
 
 const DashboardPage: NextPage = () => {
   const [content, setContent] = useState<Content>();
   const router = useRouter();
 
-  let { data, loading, error, previousData } = useQuery<ChallengeData>(
-    GET_CHALLENGE,
-    { variables: { id: router.query.id } }
-  );
-
-  if (loading) {
-    data = previousData;
-  }
-
-  if (error) {
-    console.error(error);
-    toast.error("Something went wrong!");
-  }
-
-  console.log(data);
-
   return (
     <DashboardLayout>
-      <Challenges
-        variant="list"
-        className="hidden md:flex"
-      />
+      <Challenges variant="list" className="hidden md:flex" />
       <div className="flex w-full flex-col md:mx-12 lg:mx-16">
-        {!data ? (
-          <div className="mx-4 mb-16 flex space-x-2 items-center justify-center">
-            <div className="h-8 w-64 animate-pulse rounded-lg bg-gray-500" />
-            <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
-          </div>
-        ) : (
-          <div className="mx-4 mb-16 flex items-center justify-center space-x-2">
-            <p className="items-center text-xl font-semibold md:text-2xl lg:text-3xl">
-              <span className="mr-1 bg-gradient-to-br from-pink-400 to-blue-400 bg-clip-text text-transparent">
-                {data.challenges_by_pk.days}
-              </span>
-              DaysOf<span>{data.challenges_by_pk.topic}</span>
-            </p>
-            {data.challenges_by_pk.isPublic ? (
-              <Tag>Public</Tag>
-            ) : (
-              <Tag>Private</Tag>
-            )}
-          </div>
-        )}
+        <ChallengeHeader id={router.query.id as string} />
         <WeekBar />
         <Editor content={content} onChange={setContent} className="mt-16" />
       </div>
