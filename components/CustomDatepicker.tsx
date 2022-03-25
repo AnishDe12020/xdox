e88eb7197ecfa24614ff54f77f703c07e1ce24b2
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-25 12:43:34 +0530

    feat(date-picker): used custom header components

--- components/CustomDatepicker.tsx
+++ components/CustomDatepicker.tsx
@@ -1,3 +1,5 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
+import { DateTime } from "luxon";
 import ReactDatePicker from "react-datepicker";
 import { Control, useController } from "react-hook-form";
 
@@ -19,11 +21,57 @@ const CustomDatepicker = ({
       <ReactDatePicker
         nextMonthButtonLabel=">"
         previousMonthButtonLabel="<"
-        popperClassName="react-datepicker-left"
+        popperClassName="react-datepicker-left transition duration-200"
         name={rhfName}
         selected={value}
         onChange={onChange}
         ref={ref}
+        renderCustomHeader={({
+          date,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }) => (
+          <div className="flex items-center justify-between px-2 py-2">
+            <span className="text-lg text-accent">
+              {DateTime.fromJSDate(date).toFormat("DDD")}
+            </span>
+
+            <div className="space-x-2">
+              <button
+                onClick={decreaseMonth}
+                disabled={prevMonthButtonDisabled}
+                type="button"
+                className={`
+                                            ${
+                                              prevMonthButtonDisabled &&
+                                              "cursor-not-allowed opacity-50"
+                                            }
+                                            inline-flex rounded-md bg-secondary p-1 text-sm font-medium text-gray-300 shadow-sm hover:bg-primary focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-opacity-60
+                                        `}
+              >
+                <ChevronLeftIcon className="h-5 w-5 text-gray-300" />
+              </button>
+
+              <button
+                onClick={increaseMonth}
+                disabled={nextMonthButtonDisabled}
+                type="button"
+                className={`
+                                            ${
+                                              nextMonthButtonDisabled &&
+                                              "cursor-not-allowed opacity-50"
+                                            }
+                                                                                        inline-flex rounded-md bg-secondary p-1 text-sm font-medium text-gray-300 shadow-sm hover:bg-primary focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-opacity-60
+
+                                        `}
+              >
+                <ChevronRightIcon className="h-5 w-5 text-gray-300" />
+              </button>
+            </div>
+          </div>
+        )}
       />
     </div>
   );
