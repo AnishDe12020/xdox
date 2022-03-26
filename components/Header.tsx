2e2682ef773eaac703879d02aecef0781874cc21
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-26 15:06:32 +0530

    style(header): add vertical padding and change horizontal margin to padding

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,19 +4,19 @@ import UserPopover from "./UserPopover";
 
 const Header = (): JSX.Element => {
   return (
-    <nav className="mx-8 flex justify-end md:mx-16 lg:mx-24">
+    <nav className="flex justify-end px-8 py-6 md:px-16 lg:px-24">
       <SignedIn>
         <UserPopover />
       </SignedIn>
       <SignedOut>
-        <Link href="/sign-up" passHref>
-          <a className="rounded-lg bg-blue-600 px-4 py-2 transition duration-200 hover:opacity-60">
-            Sign Up
-          </a>
-        </Link>
         <Link href="/sign-in" passHref>
           <a className="px-4 py-2 transition duration-200 hover:opacity-60">
             Sign In
+          </a>
+        </Link>
+        <Link href="/sign-up" passHref>
+          <a className="rounded-lg bg-blue-600 px-4 py-2 transition duration-200 hover:opacity-60">
+            Sign Up
           </a>
         </Link>
       </SignedOut>
