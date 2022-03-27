c84455dd533c70aff80ee71f1375cd60aa892ed9
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-27 16:11:40 +0530

    style(header): put auth components in a div

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,27 +5,29 @@ import UserPopover from "./UserPopover";
 
 const Header = (): JSX.Element => {
   return (
-    <nav className="flex justify-between px-8 py-6 md:px-16 lg:px-24">
+    <nav className="flex items-center justify-between px-8 py-6 md:px-16 lg:px-24">
       <Link href="/" passHref>
         <a className="transition duration-200 hover:opacity-60">
           <Logo className="h-16 w-16 md:ml-4" />
         </a>
       </Link>
-      <SignedIn>
-        <UserPopover />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/sign-in" passHref>
-          <a className="px-4 py-2 transition duration-200 hover:opacity-60">
-            Sign In
-          </a>
-        </Link>
-        <Link href="/sign-up" passHref>
-          <a className="rounded-lg bg-blue-600 px-4 py-2 transition duration-200 hover:opacity-60">
-            Sign Up
-          </a>
-        </Link>
-      </SignedOut>
+      <div className="flex flex-row items-center space-x-4">
+        <SignedIn>
+          <UserPopover />
+        </SignedIn>
+        <SignedOut>
+          <Link href="/sign-in" passHref>
+            <a className="px-4 py-2 transition duration-200 hover:opacity-60">
+              Sign In
+            </a>
+          </Link>
+          <Link href="/sign-up" passHref>
+            <a className="h-fit rounded-lg bg-blue-600 px-4 py-2 transition duration-200 hover:opacity-60">
+              Sign Up
+            </a>
+          </Link>
+        </SignedOut>
+      </div>
     </nav>
   );
 };
