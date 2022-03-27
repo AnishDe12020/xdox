a7aad139da5964ce9dd2ffe5dbcca2712cdb2270
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-27 17:39:43 +0530

    style(header): make the text sizes and margins responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,11 +9,11 @@ const Header = (): JSX.Element => {
       <div className="flex flex-row items-center space-x-4">
         <Link href="/" passHref>
           <a className="transition duration-200 hover:opacity-60">
-            <Logo className="h-16 w-16 md:ml-4" />
+            <Logo className="h-8 w-8 md:h-12 md:w-12 lg:h-16 lg:w-16 lg:ml-4" />
           </a>
         </Link>
         <Link href="/dashboard" passHref>
-          <a className="transition duration-200 hover:opacity-60">Dashboard</a>
+          <a className="transition duration-200 hover:opacity-60 text-sm md:text-md">Dashboard</a>
         </Link>
       </div>
       <div className="flex flex-row items-center">
@@ -23,12 +23,12 @@ const Header = (): JSX.Element => {
         <SignedOut>
           <div className="flex flex-row items-center space-x-4">
             <Link href="/sign-in" passHref>
-              <a className="px-4 py-2 transition duration-200 hover:opacity-60">
+              <a className="px-4 py-2 transition duration-200 text-sm md:text-md hover:opacity-60">
                 Sign In
               </a>
             </Link>
             <Link href="/sign-up" passHref>
-              <a className="h-fit rounded-lg bg-blue-600 px-4 py-2 transition duration-200 hover:opacity-60">
+              <a className="h-fit rounded-lg bg-blue-600 px-4 py-2 text-sm md:text-md transition duration-200 hover:opacity-60">
                 Sign Up
               </a>
             </Link>
