de30123feb293c20213e9aeebf37cb5e3b1527ca
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-28 14:38:36 +0530

    fix(user-popover): fixed link to edit profile page

--- components/UserPopover.tsx
+++ components/UserPopover.tsx
@@ -44,7 +44,7 @@ const UserPopover = (): JSX.Element => {
             <div className="text-center text-lg font-bold text-accent">
               {user.username}
             </div>
-            <Link href="/proifle" passHref>
+            <Link href="/profile" passHref>
               <a className="rounded-lg bg-secondary px-4 py-2 transition duration-200 hover:opacity-60">
                 Edit Profile
               </a>
