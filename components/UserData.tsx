bf77e4a240308dd1353e4ee8a6c7332834ba17eb
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-28 18:33:00 +0530

    refactor(user-page): move user data to a new component

---
+++ components/UserData.tsx
@@ -0,0 +1,65 @@
+import { GlobeIcon } from "@radix-ui/react-icons";
+import { UserData } from "../types/User";
+import GitHubLogo from "./Icons/GitHub";
+import TwitterLogo from "./Icons/Twitter";
+
+interface IUserDataProps {
+  userData: UserData;
+}
+
+const UserDataComponent = ({ userData }: IUserDataProps): JSX.Element => {
+  return userData ? (
+    <>
+      <img
+        src={userData.profile_image_url}
+        className="h-32 w-32 rounded-full"
+        alt={userData.username}
+      />
+      <p className="text-xl font-semibold md:text-2xl lg:text-3xl">
+        {userData.first_name} {userData.last_name}
+      </p>
+      <p className="text-md md:text-lg lg:text-xl">@{userData.username}</p>
+      <p>{userData.bio}</p>
+      <div className="mt-4 flex flex-row space-x-4">
+        <a
+          href={`https://twitter.com/${userData.twitter_username}`}
+          className="transition duration-200 hover:opacity-60"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TwitterLogo className="h-8 w-8" />
+        </a>
+        <a
+          href={`https://github.com/${userData.github_username}`}
+          className="transition duration-200 hover:opacity-60"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubLogo className="h-8 w-8" />
+        </a>
+        <a
+          href={userData.website_url}
+          className="transition duration-200 hover:opacity-60"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GlobeIcon className="h-8 w-8" />
+        </a>
+      </div>
+    </>
+  ) : (
+    <>
+      <div className="h-32 w-32 animate-pulse rounded-full bg-secondary" />
+      <div className="h-8 w-48 animate-pulse rounded-lg bg-secondary" />
+      <div className="h-6 w-32 animate-pulse rounded-lg bg-secondary" />
+      <div className="h-4 w-64 animate-pulse rounded-lg bg-secondary" />
+      <div className="mt-4 flex flex-row space-x-4">
+        <div className="h-8 w-8 animate-pulse rounded-full bg-secondary" />
+        <div className="h-8 w-8 animate-pulse rounded-full bg-secondary" />
+        <div className="h-8 w-8 animate-pulse rounded-full bg-secondary" />
+      </div>
+    </>
+  );
+};
+
+export default UserDataComponent;
--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -6,6 +6,7 @@ import toast from "react-hot-toast";
 import ChallengeData from "../../components/ChallengeData";
 import GitHubLogo from "../../components/Icons/GitHub";
 import TwitterLogo from "../../components/Icons/Twitter";
+import UserDataComponent from "../../components/UserData";
 import { GET_CHALLENGES, GET_USER_DATA } from "../../graphql/queries";
 import { UnauthenticatedApolloProviderWrapper } from "../../lib/apolloClientUnauthenticated";
 import { UserData } from "../../types/User";
@@ -33,67 +34,14 @@ const UserPage: NextPage<IUserPageProps> = ({ userData }: IUserPageProps) => {
   return (
     <UnauthenticatedApolloProviderWrapper>
       <div className="mx-8 flex flex-col items-center space-y-4 md:mx-16 lg:mx-32">
-        {userData ? (
-          <>
-            <img
-              src={userData.profile_image_url}
-              className="h-32 w-32 rounded-full"
-              alt={userData.username}
-            />
-            <p className="text-xl font-semibold md:text-2xl lg:text-3xl">
-              {userData.first_name} {userData.last_name}
-            </p>
-            <p className="text-md md:text-lg lg:text-xl">
-              @{userData.username}
-            </p>
-            <p>{userData.bio}</p>
-            <div className="mt-4 flex flex-row space-x-4">
-              <a
-                href={`https://twitter.com/${userData.twitter_username}`}
-                className="transition duration-200 hover:opacity-60"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <TwitterLogo className="h-8 w-8" />
-              </a>
-              <a
-                href={`https://github.com/${userData.github_username}`}
-                className="transition duration-200 hover:opacity-60"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GitHubLogo className="h-8 w-8" />
-              </a>
-              <a
-                href={userData.website_url}
-                className="transition duration-200 hover:opacity-60"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GlobeIcon className="h-8 w-8" />
-              </a>
-            </div>
+        <UserDataComponent userData={userData} />
 
-            <ChallengeData
-              variant="grid"
-              challengeData={challengesData}
-              handleChallengeClick={() => console.log("e")}
-              readonly
-            />
-          </>
-        ) : (
-          <>
-            <div className="h-32 w-32 animate-pulse rounded-full bg-secondary" />
-            <div className="h-8 w-48 animate-pulse rounded-lg bg-secondary" />
-            <div className="h-6 w-32 animate-pulse rounded-lg bg-secondary" />
-            <div className="h-4 w-64 animate-pulse rounded-lg bg-secondary" />
-            <div className="mt-4 flex flex-row space-x-4">
-              <div className="h-8 w-8 animate-pulse rounded-full bg-secondary" />
-              <div className="h-8 w-8 animate-pulse rounded-full bg-secondary" />
-              <div className="h-8 w-8 animate-pulse rounded-full bg-secondary" />
-            </div>
-          </>
-        )}
+        <ChallengeData
+          variant="grid"
+          challengeData={challengesData}
+          handleChallengeClick={() => console.log("e")}
+          readonly
+        />
       </div>
     </UnauthenticatedApolloProviderWrapper>
   );
