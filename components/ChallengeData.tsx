7d34ac7c00b9272542c6ed5b39eb7e59a44bf9f5
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-28 15:56:03 +0530

    refactor(challenges): move challenges display to a new component

---
+++ components/ChallengeData.tsx
@@ -0,0 +1,98 @@
+import classNames from "classnames";
+import { ChallengeData, ChallengesData } from "../types/Challenges";
+import CreateChallenge from "./CreateChallenge";
+import Tag from "./Tag";
+
+interface IChallengeDataProps {
+  variant?: "grid" | "list";
+  className?: string;
+  activeChallengeId?: string;
+  handleChallengeClick: (challengeId: string) => void;
+  challengeData?: ChallengesData;
+  readonly?: boolean;
+}
+
+const ChallengeData = ({
+  variant,
+  className,
+  activeChallengeId,
+  handleChallengeClick,
+  challengeData,
+  readonly,
+}: IChallengeDataProps): JSX.Element => {
+  return (
+    <div
+      className={classNames(
+        "flex flex-col",
+        variant === "list" ? "w-80 items-start" : "w-full",
+        className
+      )}
+    >
+      {!readonly && (
+        <CreateChallenge
+          className={variant === "grid" ? "w-fit self-end" : ""}
+        />
+      )}
+      <div
+        className={classNames(
+          variant === "list"
+            ? "flex w-full flex-col items-start space-y-8"
+            : "grid items-center justify-center gap-12",
+          "mt-8"
+        )}
+        style={{
+          gridTemplateColumns:
+            variant === "grid" ? "repeat(auto-fit, minmax(300px, 1fr))" : "",
+        }}
+      >
+        {!challengeData ? (
+          <>
+            <div className="flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4">
+              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
+              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
+            </div>
+            <div className="flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4">
+              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
+              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
+            </div>
+            <div className="flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4">
+              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
+              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
+            </div>
+            <div className="flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4">
+              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
+              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
+            </div>
+            <div className="flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4">
+              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
+              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
+            </div>
+          </>
+        ) : challengeData.challenges.length > 0 ? (
+          challengeData.challenges.map(challenge => (
+            <button
+              className={classNames(
+                "flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4 transition duration-200 hover:opacity-60",
+                activeChallengeId === challenge.id && "border-4 border-blue-500"
+              )}
+              key={challenge.id}
+              onClick={() => handleChallengeClick(challenge.id as string)}
+            >
+              <p className="text-lg font-semibold">
+                <span className="mr-1 bg-gradient-to-br from-pink-400 to-blue-400 bg-clip-text text-transparent">
+                  {challenge.days}
+                </span>
+                DaysOf<span>{challenge.topic}</span>
+              </p>
+              {challenge.isPublic ? <Tag>Public</Tag> : <Tag>Private</Tag>}
+            </button>
+          ))
+        ) : (
+          <p className="text-semibold text-center text-lg">No Challenges yet</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ChallengeData;
--- components/Challenges.tsx
+++ components/Challenges.tsx
@@ -1,11 +1,9 @@
 import { useQuery } from "@apollo/client";
-import classNames from "classnames";
 import { useRouter } from "next/router";
 import toast from "react-hot-toast";
 import { GET_CHALLENGES } from "../graphql/queries";
-import { Challenge, ChallengesData } from "../types/Challenges";
-import CreateChallenge from "./CreateChallenge";
-import Tag from "./Tag";
+import { ChallengesData } from "../types/Challenges";
+import ChallengeData from "./ChallengeData";
 
 interface IChallengesProps {
   variant?: "grid" | "list";
@@ -35,78 +33,18 @@ const Challenges = ({
     toast.error("Error loading challenges");
   }
 
-  const handleChallengeClick = (challenge: Challenge) => {
-    router.push(`/dashboard/${challenge.id}/1`);
+  const handleChallengeClick = (challengeId: string) => {
+    router.push(`/dashboard/${challengeId}/1`);
   };
 
   return (
-    <div
-      className={classNames(
-        "flex flex-col",
-        variant === "list" ? "w-80 items-start" : "w-full",
-        className
-      )}
-    >
-      <CreateChallenge className={variant === "grid" ? "w-fit self-end" : ""} />
-      <div
-        className={classNames(
-          variant === "list"
-            ? "flex w-full flex-col items-start space-y-8"
-            : "grid items-center justify-center gap-12",
-          "mt-8"
-        )}
-        style={{
-          gridTemplateColumns:
-            variant === "grid" ? "repeat(auto-fit, minmax(300px, 1fr))" : "",
-        }}
-      >
-        {!data ? (
-          <>
-            <div className="flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4">
-              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
-              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
-            </div>
-            <div className="flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4">
-              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
-              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
-            </div>
-            <div className="flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4">
-              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
-              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
-            </div>
-            <div className="flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4">
-              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
-              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
-            </div>
-            <div className="flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4">
-              <div className="h-8 w-32 animate-pulse rounded-lg bg-gray-500" />
-              <div className="h-6 w-16 animate-pulse rounded-lg bg-gray-500" />
-            </div>
-          </>
-        ) : data && data.challenges.length > 0 ? (
-          data.challenges.map(challenge => (
-            <button
-              className={classNames(
-                "flex h-32 w-full flex-col space-y-2 rounded-lg bg-secondary p-4 transition duration-200 hover:opacity-60",
-                activeChallengeId === challenge.id && "border-4 border-blue-500"
-              )}
-              key={challenge.id}
-              onClick={() => handleChallengeClick(challenge)}
-            >
-              <p className="text-lg font-semibold">
-                <span className="mr-1 bg-gradient-to-br from-pink-400 to-blue-400 bg-clip-text text-transparent">
-                  {challenge.days}
-                </span>
-                DaysOf<span>{challenge.topic}</span>
-              </p>
-              {challenge.isPublic ? <Tag>Public</Tag> : <Tag>Private</Tag>}
-            </button>
-          ))
-        ) : (
-          <p className="text-semibold text-center text-lg">No Challenges yet</p>
-        )}
-      </div>
-    </div>
+    <ChallengeData
+      className={className}
+      variant={variant}
+      activeChallengeId={activeChallengeId}
+      handleChallengeClick={handleChallengeClick}
+      challengeData={data}
+    />
   );
 };
 
