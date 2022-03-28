dd616d89129f6fc8da572976cc61381695d2236a
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-28 14:40:51 +0530

    chore(user-page): added profile image, name and username

--- pages/[username].tsx
+++ pages/[username].tsx
@@ -32,7 +32,35 @@ const UserPage: NextPage = () => {
 
   console.log(userData);
 
-  return <h1>User Profile</h1>;
+  return (
+    <div className="mx-8 flex flex-col md:mx-16 lg:mx-32">
+      {userData?.users ? (
+        <div className="flex flex-row space-x-4">
+          <img
+            src={userData.users[0].profile_image_url}
+            className="h-32 w-32 rounded-full"
+            alt={userData.users[0].username}
+          />
+          <div className="flex flex-col space-y-2">
+            <p className="text-xl font-semibold md:text-2xl lg:text-3xl">
+              {userData.users[0].first_name} {userData.users[0].last_name}
+            </p>
+            <p className="text-md md:text-lg lg:text-xl">
+              @{userData.users[0].username}
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-row space-x-4">
+          <div className="h-32 w-32 animate-pulse rounded-full bg-secondary" />
+          <div className="flex flex-col space-y-2">
+            <div className="h-8 w-48 animate-pulse rounded-lg bg-secondary" />
+            <div className="h-6 w-32 animate-pulse rounded-lg bg-secondary" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserPage;
