0634034f455b66eae34e3c6dadb777acb880be63
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-28 15:47:54 +0530

    chore: add the user type (had forgotten to add)

---
+++ types/User.ts
@@ -0,0 +1,12 @@
+interface UserData {
+  first_name: string;
+  last_name: string;
+  twitter_username: string;
+  github_username: string;
+  website_url: string;
+  bio: string;
+  username: string;
+  profile_image_url: string;
+}
+
+export type { UserData };
