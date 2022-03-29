3fd5e1bfe608e926e53040a56b1a9b86445b2947
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-29 19:05:32 +0530

    chore(home): finished landing page hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -13,8 +15,24 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col justify-center items-center">
-        <h1 className="lg:text-5xl md:text-4xl text-3xl font-bold">A <span className="from-teal-500 to-blue-500 text-clip text-transparent bg-clip-text bg-gradient-to-tr animate-gradient-text">better</span> way to keep track of your challenges</h1>
+      <main className="mt-16 flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl">
+          A{" "}
+          <span className="bg-gradient-to-tr from-teal-500 to-blue-500 bg-clip-text text-transparent">
+            better
+          </span>{" "}
+          way to keep track of your challenges
+        </h1>
+        <h2 className="mt-8 font-semibold text-xl md:text-2xl lg:text-3xl">
+          XdoX provides you a way to start challenges and log your progress
+          everyday. 
+        </h2>
+        <h3 className="text-lg md:text-xl lg:text-2xl text-normal mt-4">You even get a{" "}
+          <span className="bg-gradient-to-br from-lime-500 to-green-500 bg-clip-text text-transparent font-bold">
+            unique profile page
+          </span>{" "}
+          🤩</h3>
+        <Link href="/sign-up" passHref><a className="bg-accent text-black rounded-lg px-4 py-2 transition duration-200 hover:opacity-60 mt-4 flex flex-row space-x-2 group justify-center items-center"><span>Sounds good, sign me up</span><ArrowRightIcon className="h-4 w-4 group-hover:translate-x-1 transition duration-200" /></a></Link>
       </main>
     </div>
   );
