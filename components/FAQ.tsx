609990f61a93e7d01f260e5737ed6d43c3bf9830
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-29 22:03:41 +0530

    style(faq): make question bigger

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -6,7 +6,7 @@ interface IFAQProps {
 const FAQ = ({ title, content }: IFAQProps): JSX.Element => {
   return (
     <div className="flex flex-col space-y-2">
-      <h3>{title}</h3>
+      <h3 className="text-lg md:text-xl lg:text-2xl">{title}</h3>
       <p>{content}</p>
     </div>
   );
