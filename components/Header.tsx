8e7dac8a0f0dac4fa8fbcb99cf7fa18b8de7c501
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-31 21:13:24 +0530

    style(header): fixed responsive sizes

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,25 +27,25 @@ const Header = (): JSX.Element => {
       </div>
       <div className="flex flex-row items-center">
         <a
-          className="mr-4 p-2 transition duration-200 hover:opacity-60"
+          className="mr-2 p-2 transition duration-200 hover:opacity-60 md:mr-2"
           href="https://github.com/AnishDe12020/xdox"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <GitHubLogo className="h-6 w-6" />
+          <GitHubLogo className="h-4 w-4 md:h-6 md:w-6" />
         </a>
         <SignedIn>
           <UserPopover />
         </SignedIn>
         <SignedOut>
-          <div className="flex flex-row items-center space-x-4">
+          <div className="flex flex-row items-center space-x-2 md:space-x-4">
             <Link href="/sign-in" passHref>
-              <a className="md:text-md px-4 py-2 text-sm transition duration-200 hover:opacity-60">
+              <a className="md:text-md px-2 py-2 text-sm transition duration-200 hover:opacity-60 md:px-4">
                 Sign In
               </a>
             </Link>
             <Link href="/sign-up" passHref>
-              <a className="md:text-md h-fit rounded-lg bg-blue-600 px-4 py-2 text-sm transition duration-200 hover:opacity-60">
+              <a className="md:text-md h-fit rounded-lg bg-blue-600 px-2 py-2 text-sm transition duration-200 hover:opacity-60 md:px-4">
                 Sign Up
               </a>
             </Link>
